bd1235a3e08a78b6714b83379cdb56bbcc6274b0
goldmedal <liugs963@gmail.com>
2015-03-10 23:21:55 +0800

    Using thumb2 conditional instruction to decrease the number of lines of lab-1
    Practice method of application of thumb2 conditional instruction, IT.

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -15,13 +15,20 @@ fibonacci:
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
-   subs r4, r0, #0
-   ble .L3
+   mov r4, r0 
+   cmp r4, #0
+   itt ls
+	movls r0, #0			@ R0 = 0
+	popls {r3, r4, r5, pc}		@ EPILOG
+    
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+
    cmp r4, #1
-   beq .L4
+   itt eq
+   moveq r0, #1
+   popeq {r3, r4, r5, pc}
 
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
@@ -33,6 +40,7 @@ fibonacci:
 	@ Recursive call to fibonacci with R4 - 2 as parameter
    mov r5, r0
    sub r0, r4, #2
+   bl fibonacci
 
 	@ R0 = R5 + R0 (update flags)
    add r0, r5, r0
@@ -40,13 +48,6 @@ fibonacci:
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
